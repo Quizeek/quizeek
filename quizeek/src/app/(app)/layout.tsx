e75cc19831e49bf16d36517db3690f0172577371
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2024-12-08 18:03:00 +0100

    feat: Modified metadata

---
+++ quizeek/src/app/(app)/layout.tsx
@@ -0,0 +1,9 @@
+import React, { PropsWithChildren } from 'react';
+
+const Layout = (props: PropsWithChildren) => {
+  const { children } = props;
+
+  return <div>{children}</div>;
+};
+
+export default Layout;
