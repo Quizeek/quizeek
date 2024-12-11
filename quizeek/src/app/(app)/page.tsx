cf3061c356c44523affacba2857bbf1e1f7f4fbf
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2024-12-11 20:28:16 +0100

    wip: Fix build error

--- quizeek/src/app/(app)/page.tsx
+++ quizeek/src/app/(app)/page.tsx
@@ -1,8 +1,9 @@
-import Loader from '@/components/layout/loader';
+import QuizAuthorBadge from '@/components/quiz/quiz-author-badge';
 import { QuizList } from '@/components/quiz/quiz-list';
 import { SearchInput } from '@/components/search-input';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { getActiveQuizes } from '@/db/queries';
 import { toNormalizedString } from '@/utils';
-import { Suspense } from 'react';
 
 type HomeProps = {
   searchParams?: Promise<{
@@ -14,6 +15,8 @@ const Home = async ({ searchParams }: HomeProps) => {
   const searchText = (await searchParams)?.searchText || '';
   const normalizedSearchText = toNormalizedString(searchText);
 
+  const quizes = await getActiveQuizes(normalizedSearchText);
+
   return (
     <div className="flex flex-col gap-3">
       <SearchInput
@@ -22,9 +25,12 @@ const Home = async ({ searchParams }: HomeProps) => {
         className="w-full md:w-64"
       />
 
-      <Suspense fallback={<Loader />}>
-        <QuizList searchText={normalizedSearchText} onlyActive={false} />
-      </Suspense>
+      <ScrollArea className="h-[calc(100vh-9rem)]" type="always">
+        <QuizList
+          quizes={quizes}
+          getQuizBadge={(quiz) => <QuizAuthorBadge quiz={quiz} />}
+        />
+      </ScrollArea>
     </div>
   );
 };
