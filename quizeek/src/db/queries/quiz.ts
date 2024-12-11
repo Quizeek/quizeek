00dcf8815f30be5d218916468c210730b9ccef08
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2024-12-11 20:29:46 +0100

    wip: Fix build error

--- quizeek/src/db/queries/quiz.ts
+++ quizeek/src/db/queries/quiz.ts
@@ -5,23 +5,18 @@ import { and, eq, like, or } from 'drizzle-orm';
 import { db } from '..';
 import { quizes, QuizWithUser } from '../schema/quiz';
 
-export const getFilteredQuizes = async (
-  search: string,
-  onlyActive: boolean,
-  createdBy?: string
+export const getActiveQuizes = async (
+  search: string
 ): Promise<QuizWithUser[]> => {
   try {
-    const whereClause = and(
-      or(
-        like(quizes.title, `%${search}%`),
-        like(quizes.description, `%${search}%`)
-      ),
-      ...(onlyActive ? [eq(quizes.isActive, true)] : []),
-      ...(createdBy ? [like(quizes.createdBy, `%${createdBy}%`)] : [])
-    );
-
     const dbQuizes = await db.query.quizes.findMany({
-      where: whereClause,
+      where: and(
+        or(
+          like(quizes.title, `%${search}%`),
+          like(quizes.description, `%${search}%`)
+        ),
+        eq(quizes.isActive, true)
+      ),
       with: { creator: true },
     });
 
