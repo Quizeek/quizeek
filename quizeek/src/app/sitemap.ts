127bf8379a1525d9c9e0a0a8d8dfb552833ef2a3
Tomáš Štucka <Tomas00217@gmail.com>
2024-12-15 17:23:13 +0100

    fix: add query with try catch

--- quizeek/src/app/sitemap.ts
+++ quizeek/src/app/sitemap.ts
@@ -1,6 +1,23 @@
+import { getTopQuizesByAttempts } from '@/db/queries';
+import { Quiz } from '@/db/schema/quiz';
 import { MetadataRoute } from 'next';
 
 export const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
+  let quizes: Quiz[] = [];
+  try {
+    quizes = await getTopQuizesByAttempts(5);
+  } catch {
+    // eslint-disable-next-line no-console
+    console.warn(
+      'Failed to fetch quizes, this might be due to invalid env settings defaulting to an empty list.'
+    );
+  }
+
+  const quizPaths = quizes.map((quiz) => ({
+    url: `https://quizeek.vercel.app/quiz/${quiz.id}`,
+    lastModified: new Date().toISOString(),
+  }));
+
   return [
     {
       url: 'https://quizeek.vercel.app/',
@@ -14,6 +31,7 @@ export const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
       url: 'https://quizeek.vercel.app/auth/quiz/create',
       lastModified: new Date().toISOString(),
     },
+    ...quizPaths,
   ];
 };
 
